a4f6c95eefe3aa1ee45bd3ab2b6288f9b4e07f46
Virendra Nishad <virendran818@gmail.com>
2025-04-02 23:23:54 +0530

    unfollow backend fix

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -37,7 +37,6 @@ export const getUserProfile = async (req: Request, res: Response) => {
 
 export const followUser = async (req: Request, res: Response) => {
   try {
-    console.log('userToFollow: ', req.params.id);
     const userToFollow = await User.findById(req.params.id);
 
     const authHeader = req.headers.authorization;
@@ -50,7 +49,6 @@ export const followUser = async (req: Request, res: Response) => {
     if (!decoded) return res.status(401).json({ message: 'Invalid token' });
 
     const currentUserId = decoded.userId;
-    console.log(decoded, currentUserId);
 
     const currentUser = await User.findById(currentUserId);
 
@@ -63,7 +61,7 @@ export const followUser = async (req: Request, res: Response) => {
       await userToFollow.save();
       await currentUser.save();
     }
-
+    console.log('follow user');
     res.sendStatus(200);
   } catch (err) {
     res.status(500).send('Server Error');
@@ -73,16 +71,27 @@ export const followUser = async (req: Request, res: Response) => {
 export const unfollowUser = async (req: Request, res: Response) => {
   try {
     const userToUnfollow = await User.findById(req.params.id);
-    const currentUser = await User.findById(req.user._id);
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith('Bearer '))
+      return res.status(401).json({ message: 'Unauthorized' });
+
+    const token = authHeader.split(' ')[1];
+    const decoded = decodeToken(token);
+
+    if (!decoded) return res.status(401).json({ message: 'Invalid token' });
+
+    const currentUserId = decoded.userId;
+    const currentUser = await User.findById(currentUserId);
 
     if (!userToUnfollow || !currentUser)
       return res.status(404).send('User not found');
 
-    userToUnfollow.followers.pull(req.user._id);
+    userToUnfollow.followers.pull(currentUserId);
     currentUser.following.pull(req.params.id);
     await userToUnfollow.save();
     await currentUser.save();
-
+    console.log('unfollow user');
     res.sendStatus(200);
   } catch (err) {
     res.status(500).send('Server Error');
